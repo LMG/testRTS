7e73dce8cf4a3b22998e930f71a8a6f2a3ea5f4c
Dupre Antoine <dupre.antoine.nicolas@gmail.com>
2011-09-22 15:27:16 +0200

    Drag and drop with the mouse

--- main.c
+++ main.c
@@ -15,52 +15,76 @@ int main(int argc, char *argv[])
     int keepPlaying = 1;
     SDL_Rect origin = {0,0};
     int value = 0;//stupid test
+
+	int move=0;
+
     
     while (keepPlaying)
     {
-        SDL_PollEvent(&event);
-        switch(event.type)
+    	
+        while(SDL_PollEvent(&event))
         {
-            case SDL_QUIT:
-                keepPlaying = 0;
-                break;
-            case SDL_KEYDOWN:
-                switch(event.key.keysym.sym)
-                {
-                    case SDLK_ESCAPE:
-                        keepPlaying = 0;
-                        break;
-                    case SDLK_RIGHT:
-                    	origin.x +=5;
-                    	break;
-                   	case SDLK_LEFT:
-                    	origin.x -=5;
-                   		break;
-                   	case SDLK_UP:
-                    	origin.y -=5;
-                   		break;
-                   	case SDLK_DOWN:
-                    	origin.y +=5;
-                   		break;
-                   	//stupid test
-                   	case SDLK_a:
-                   		value++;
-                   		break;
-                   	case SDLK_z:
-                   		value--;
-                   		break;
-                    default: 
-                    	break;
-                }
-                break;
-           default:
-            	break;
-        }
-        
+		    switch(event.type)
+		    {
+				// button press
+				case SDL_MOUSEBUTTONDOWN:
+					move = 1;
+					
+				
+					break;
+				case SDL_MOUSEBUTTONUP:
+					move = 0;
+					break;			
+				case SDL_MOUSEMOTION:
+					if(move==1)
+					{
+						origin.x = origin.x+event.motion.xrel;
+						origin.y = origin.y+event.motion.yrel;
+						
+						
+					}
+					break;								
+		        case SDL_QUIT:
+		            keepPlaying = 0;
+		            break;
+		        case SDL_KEYDOWN:
+		            switch(event.key.keysym.sym)
+		            {
+		                case SDLK_ESCAPE:
+		                    keepPlaying = 0;
+		                    break;
+		                case SDLK_RIGHT:
+		                	origin.x +=5;
+		                	break;
+		               	case SDLK_LEFT:
+		                	origin.x -=5;
+		               		break;
+		               	case SDLK_UP:
+		                	origin.y -=5;
+		               		break;
+		               	case SDLK_DOWN:
+		                	origin.y +=5;
+		               		break;
+		               	//stupid test
+		               	case SDLK_a:
+		               		value++;
+		               		break;
+		               	case SDLK_z:
+		               		value--;
+		               		break;
+			
+		                default: 
+		                	break;
+		            }
+		            break;
+		       default:
+		        	break;
+		    }
+		}    
         game(screen, &origin, &value);
         
         //waiting
-        SDL_Delay(DELAY);
+        //SDL_Delay(DELAY);
     }
     
     SDL_Quit();
