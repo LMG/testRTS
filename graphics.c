7753d4f20f1f68300852dd0c464a685f6c4a075c
Louis-Marie Givel <louismariegivel@gmail.com>
2011-09-22 15:37:52 +0200

    Blitting the tiles in the right order + loading them and the new ones the right way (should have been another commit

--- graphics.c
+++ graphics.c
@@ -16,28 +16,29 @@ void game (SDL_Surface *screen, SDL_Rect* origin, int *value)
 	//initialisation
 	static int init = 0;
     
-	static SDL_Surface *tile;
-	static SDL_Rect origin_tile;
-	static SDL_Surface *tile2;
-	static SDL_Rect origin_tile2;
+	static SDL_Surface *tile[NB_TILES];
+	static SDL_Rect origin_tile[NB_TILES];
 	static SDL_Rect boardSize;
 	
 	if(!init)
 	{
 		init =1;
 		
-		tile = IMG_Load("ressources/tile_001.png");
-		SDL_SetColorKey(tile, SDL_SRCCOLORKEY, SDL_MapRGB(screen->format, 221, 0, 221));
-		origin_tile.x = 0;
-		origin_tile.y = tile->h/2;
+		int i;
 		
-		tile2 = IMG_Load("ressources/tile_002.png");
-		SDL_SetColorKey(tile2, SDL_SRCCOLORKEY, SDL_MapRGB(screen->format, 221, 0, 221));
-		origin_tile2.x = 0;
-		origin_tile2.y = tile2->h-(tile->h/2);
+		for(i=0; i<NB_TILES; i++)
+		{
+			char name[24] = "ressources/tile_";
+			sprintf(name, "%s%03d%s", name, i, ".png");
+			printf("%s\n", name);
+			tile[i] = IMG_Load(name);
+			SDL_SetColorKey(tile[i], SDL_SRCCOLORKEY, SDL_MapRGB(screen->format, 221, 0, 221));
+			origin_tile[i].x = 0;
+			origin_tile[i].y = tile[i]->h-TILE_HEIGHT;
+		}
 		
-		boardSize.x = 30;
-		boardSize.y = 30;
+		boardSize.x = 3;
+		boardSize.y = 4;
 		
 		printf("Init done \n");
 	}
@@ -46,26 +47,69 @@ void game (SDL_Surface *screen, SDL_Rect* origin, int *value)
     SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
     
     //blitting the tiles TODO: load the tiles from a matrix
-    int x, y;
+    int x, y, n, i, numberOfLine = (boardSize.x+boardSize.y-1) ;
     SDL_Rect position;
-    for (x=0; x<boardSize.x; x++)
+    for (n=1; n<=numberOfLine; n++)
     {
-    	for (y=0; y<boardSize.y; y++)
+  		printf("%d\n", n);
+    	if(n>=boardSize.y)
     	{
-    		if((x^y)>*value)//TODO:insert the real test with the matrix instead of this stupid test
-    		{
-    			position.x = origin->x+(y+x)*(tile->w/2)-origin_tile2.x;
-	    		position.y = origin->y+(-y+x)*(tile->h/2)-origin_tile2.y; //we makes the y and x position correspond with the origin of the tile.
-	    		SDL_BlitSurface(tile2, NULL, screen, &position);
-	    	}
-	    	else
-	    	{
-	    		position.x = origin->x+(y+x)*(tile->w/2)-origin_tile.x;
-	    		position.y = origin->y+(-y+x)*(tile->h/2)-origin_tile.y; //we makes the y and x position correspond with the origin of the tile.
-	    		SDL_BlitSurface(tile, NULL, screen, &position);
-	    	}
+    		y=boardSize.y-1;
+    		x=n-boardSize.y;
     	}
+    	else
+    	{
+    		y=n-1;
+    		x=0;
+    	}
+    	for(i=0; i<n && i<boardSize.x && i<boardSize.y; i++)
+    	{
+    		printf("x,y = %d,%d\n", x, y);
+    		y--;
+    		x++;
+    		if((x^y)>*value)//TODO:insert the real test with the matrix instead of this stupid test
+			{
+				position.x = origin->x+(x-y)*(TILE_WIDTH/2)-origin_tile[3].x;
+				position.y = origin->y+(y+x)*(TILE_HEIGHT/2)-origin_tile[3].y; //we makes the y and x position correspond with the origin of the tile.
+				SDL_BlitSurface(tile[3], NULL, screen, &position);
+			}
+			else
+			{
+				position.x = origin->x+(x-y)*(TILE_WIDTH/2)-origin_tile[2].x;
+				position.y = origin->y+(y+x)*(TILE_HEIGHT/2)-origin_tile[2].y; //we makes the y and x position correspond with the origin of the tile.
+				SDL_BlitSurface(tile[2], NULL, screen, &position);
+			}
+    	}
+    	
+    	/*
+    	//
+    	if(boardSize.x-n>0)
+    	{
+    		x=boardSize.x-n;
+    	}
+    	else
+    	{
+    		x=0;
+    	}
+    	for(y=0;x<boardSize.x&&y<=n;x++,y++)
+    	{
+			//printf("x,y = %d,%d\n", x, y);
+			if((x^y)>*value)//TODO:insert the real test with the matrix instead of this stupid test
+			{
+				position.x = origin->x+(x-y)*(TILE_WIDTH/2)-origin_tile[3].x;
+				position.y = origin->y+(y+x)*(TILE_HEIGHT/2)-origin_tile[3].y; //we makes the y and x position correspond with the origin of the tile.
+				SDL_BlitSurface(tile[3], NULL, screen, &position);
+			}
+			else
+			{
+				position.x = origin->x+(x-y)*(TILE_WIDTH/2)-origin_tile[2].x;
+				position.y = origin->y+(y+x)*(TILE_HEIGHT/2)-origin_tile[2].y; //we makes the y and x position correspond with the origin of the tile.
+				SDL_BlitSurface(tile[2], NULL, screen, &position);
+			}
+    	}
+    	*/
     }
+    
     
     //updating the screen
     SDL_Flip(screen);
--- graphics.h
+++ graphics.h
@@ -3,9 +3,15 @@
 
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
+#include <string.h>
 
 #define WINDOW_HEIGHT 512
 #define WINDOW_WIDTH 1024
+
+#define TILE_HEIGHT 25
+#define TILE_WIDTH 48
+
+#define NB_TILES 6
 
 //initialization of the SDL.
 void initSDL (SDL_Surface **screen);
