ba04250d9c2e5f346f2d7250c1b0149ccfa82426
unknown <lmg@lmg-PC.(none)>
2012-01-19 15:03:43 +0100

    modif de merde

--- graphics.c
+++ graphics.c
@@ -3,7 +3,7 @@
 //initialization of the SDL.
 void initSDL (SDL_Surface **screen)
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO);//modif de merde
     SDL_WM_SetIcon(IMG_Load("ressources/icon.png"), NULL); // The icon needs to be loaded before calling SDL_SetVideoMode
     *screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF );
     SDL_WM_SetCaption("JEU", NULL);
